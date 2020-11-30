b885d9aa14d9f48c0ccde755a4ffe24483717aca
BadReek <qbrbd@mail.ru>
2020-11-30 20:33:19 +0500

    initial

---
+++ Step_2.py
@@ -0,0 +1,10 @@
+c = 30
+d = 5
+if c < d:
+ print("c меньше, чем d")
+elif c == d:
+ print("c ровняется d")
+elif c > d + 10:
+ print("разница между c и d больше 10")
+else:
+ print("c больше,чем f")
---
+++ Step_3.py
@@ -0,0 +1,9 @@
+g = 7
+h = 8
+if g < h:
+ print("g меньше,чем h")
+else:
+ if g == h:
+     print("g ровнятся h")
+ else:
+     print("g больше, чем h")
